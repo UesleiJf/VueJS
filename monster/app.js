2bdf68bed8e2b9acec34b1efac2e623a67fd6af6
uesleijf <uesleijf@gmail.com>
2020-07-23 15:29:45 -0300

    - Método startGame e desistir - app.js

--- monster/app.js
+++ monster/app.js
@@ -2,12 +2,19 @@ new Vue({
     el: '#app',
     data: {
         running: false,
-        playerLife: 10,
-        monsterLife: 0
+        playerLife: 100,
+        monsterLife: 100
     },
     computed: {
         hasResult(){
             return this.playerLife == 0 || this.monsterLife == 0
+        }
+    },
+    methods: {
+        startGame(){
+            this.running = true
+            this.playerLife = 100
+            this.monsterLife = 100
         }
     },
     watch: {
